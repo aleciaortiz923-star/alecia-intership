6c9b9d2bc50768979e1489c0e1c50a97da03b0cf
aleciaortiz923-star <alecia.ortiz923@gmail.com>
2026-03-29 18:35:17 -0400

    hot collections carousel

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,9 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
 
-const HotCollections = () => {
+const HotCollections = ({ hotCollections }) => {
+  const options = {
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  };
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -14,29 +32,35 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to="/item-details">
-                    <img src={nftImage} className="lazy img-fluid" alt="" />
-                  </Link>
+          {hotCollections.length > 0 ? (
+            <OwlCarousel className="owl-theme" {...options}>
+              {hotCollections.map((item) => (
+                <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12" key={item.id}>
+                  <div className="nft_coll">
+                    <div className="nft_wrap">
+                      <Link to={`/item-details/${item.id}`}>
+                        <img src={item.nftImage} className="lazy img-fluid" alt="" />
+                      </Link>
+                    </div>
+                    <div className="nft_coll_pp">
+                      <Link to={`/author/${item.authorId}`}>
+                        <img className="lazy pp-coll" src={item.authorImage} alt="" />
+                      </Link>
+                      <i className="fa fa-check"></i>
+                    </div>
+                    <div className="nft_coll_info">
+                      <Link to={`/explore/${item.id}`}>
+                        <h4>{item.title}</h4>
+                      </Link>
+                      <span>{item.type}</span>
+                    </div>
+                  </div>
                 </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={AuthorImage} alt="" />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <span>ERC-192</span>
-                </div>
-              </div>
-            </div>
-          ))}
+              ))}
+            </OwlCarousel>
+          ) : (
+            <p>No hot collections to display.</p>
+          )}
         </div>
       </div>
     </section>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import BrowseByCategory from "../components/home/BrowseByCategory";
 import HotCollections from "../components/home/HotCollections";
 import Landing from "../components/home/Landing";
@@ -9,7 +10,23 @@ import TopSellers from "../components/home/TopSellers";
 const Home = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
+
+    const fetchHotCollections = async () => {
+      try {
+        const response = await axios.get(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
+        );
+        setHotCollections(response.data);
+        console.log("Fetched hot collections in Home:", response.data);
+      } catch (error) {
+        console.error("Error fetching hot collections in Home:", error);
+      }
+    };
+
+    fetchHotCollections();
   }, []);
+
+  const [hotCollections, setHotCollections] = useState([]);
 
   return (
     <div id="wrapper">
@@ -17,7 +34,7 @@ const Home = () => {
         <div id="top"></div>
         <Landing />
         <LandingIntro />
-        <HotCollections />
+        <HotCollections hotCollections={hotCollections} />
         <NewItems />
         <TopSellers />
         <BrowseByCategory />
