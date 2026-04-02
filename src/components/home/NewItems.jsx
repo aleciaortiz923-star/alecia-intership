8e8a8707275a02d34f46cac56e9d23678695d448
aleciaortiz923-star <alecia.ortiz923@gmail.com>
2026-04-02 17:20:37 -0400

    explore page

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -6,6 +6,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./NewItems.css";
 import Countdown from "../Countdown";
+import NFT from "../NFT";
 
 const NewItems = () => {
   const [newItems, setNewItems] = useState([]);
@@ -59,7 +60,7 @@ const NewItems = () => {
     );
   };
 
-  const settings = {
+const settings = {
     infinite: true,
     speed: 500,
     slidesToShow: 4,
@@ -126,61 +127,9 @@ const NewItems = () => {
             <Slider {...settings}>
               {newItems.map((item) => (
                 <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12" key={item.id}>
-                  <div className="nft__item">
-                    <div className="author_list_pp">
-                      <Link
-                        to={`/author/${item.authorId}`}
-                        data-bs-toggle="tooltip"
-                        data-bs-placement="top"
-                        title={`Creator: ${item.authorName}`}
-                      >
-                        <img className="lazy" src={item.authorImage} alt={item.authorName} />
-                        <i className="fa fa-check"></i>
-                      </Link>
-                    </div>
-                    {item.expiryDate && <Countdown expiryDate={item.expiryDate} />}
-
-                    <div className="nft__item_wrap">
-                      <div className="nft__item_extra">
-                        <div className="nft__item_buttons">
-                          <button>Buy Now</button>
-                          <div className="nft__item_share">
-                            <h4>Share</h4>
-                            <a href="" target="_blank" rel="noreferrer">
-                              <i className="fa fa-facebook fa-lg"></i>
-                            </a>
-                            <a href="" target="_blank" rel="noreferrer">
-                              <i className="fa fa-twitter fa-lg"></i>
-                            </a>
-                            <a href="">
-                              <i className="fa fa-envelope fa-lg"></i>
-                            </a>
-                          </div>
-                        </div>
-                      </div>
-
-                      <Link to={`/item-details/${item.id}`}>
-                        <img
-                          src={item.nftImage}
-                          className="lazy nft__item_preview"
-                          alt=""
-                        />
-                      </Link>
-                    </div>
-                    <div className="nft__item_info">
-                      <Link to={`/item-details/${item.id}`}>
-                        <h4>{item.title}</h4>
-                      </Link>
-                      <div className="nft__item_price">{item.price} ETH</div>
-                      <div className="nft__item_like">
-                        <i className="fa fa-heart"></i>
-                        <span>{item.likes}</span>
-                      </div>
-                    </div>
-                  </div>
+                  <NFT item={item} />
                 </div>
-              ))}
-            </Slider>
+              ))}</Slider>
           ) : (
             <p>No new items to display.</p>
           )}
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -4,24 +4,54 @@ import SubHeader from "../images/subheader.jpg";
 import ExploreItems from "../components/explore/ExploreItems";
 
 const Explore = () => {
+  const [exploreItems, setExploreItems] = useState([]);
+  const [dataFetched, setDataFetched] = useState(false);
+  const [minimumDelayMet, setMinimumDelayMet] = useState(false);
+  const [filter, setFilter] = useState("");
+  const [isFiltering, setIsFiltering] = useState(false);
+  const [visibleCount, setVisibleCount] = useState(8);
+
+  const loadMore = () => {
+    setVisibleCount((prevCount) => prevCount + 4);
+  };
+
   useEffect(() => {
     window.scrollTo(0, 0);
-<h1></h1>
-    const fetchHotCollections = async () => {
+
+    const delayTimer = setTimeout(() => {
+      setMinimumDelayMet(true);
+    }, 1500);
+
+    const fetchExploreItems = async () => {
       try {
         const response = await axios.get(
-          "https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections"
+          `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore${filter ? `?filter=${filter}` : ''}`
         );
-        setHotCollections(response.data);
+        setExploreItems(response.data);
       } catch (error) {
-        console.error("Error fetching hot collections:", error);
+        console.error("Error fetching explore items:", error);
+      } finally {
+        setDataFetched(true);
       }
     };
 
-    fetchHotCollections();
-  }, []);
+    fetchExploreItems();
 
-  const [hotCollections, setHotCollections] = useState([]);
+    return () => clearTimeout(delayTimer);
+  }, [filter]);
+
+  const handleFilterChange = (e) => {
+    setIsFiltering(true);
+    setFilter(e.target.value);
+    setVisibleCount(8);
+    setTimeout(() => {
+      setIsFiltering(false);
+    }, 500);
+  };
+
+
+
+  const isLoading = !dataFetched || !minimumDelayMet || isFiltering;
 
   return (
     <div id="wrapper">
@@ -48,7 +78,22 @@ const Explore = () => {
         <section aria-label="section">
           <div className="container">
             <div className="row">
-              <ExploreItems hotCollections={hotCollections} />
+              <div className="col-lg-12">
+                <div className="items_filter">
+                  <select id="filter-items" defaultValue="" onChange={handleFilterChange}>
+                    <option value="">Default</option>
+                    <option value="price_low_to_high">Price, Low to High</option>
+                    <option value="price_high_to_low">Price, High to Low</option>
+                    <option value="likes_high_to_low">Most liked</option>
+                  </select>
+                </div>
+              </div>
+              <ExploreItems
+                exploreItems={exploreItems}
+                loading={isLoading}
+                visibleCount={visibleCount}
+                loadMore={loadMore}
+              />
             </div>
           </div>
         </section>
